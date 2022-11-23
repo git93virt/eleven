cf1d7a70f68f297cde8b33082a8331b380903df6
unknown <rraj96449@gmail.com>
2022-11-23 16:34:51 +0530

    deleted all unwanted files

--- web dev/django/practiceproject/practiceproject/views.py
+++
@@ -1,8 +0,0 @@
-
-from django.http.response import HttpResponse
-from django.shortcuts import render
-
-
-def home(request):
-    # return HttpResponse("<h1><center>Home page</center></h1>")
-    return render(request,"home.html")
--- web dev/django/rjsite/rjsite/first.py
+++
@@ -1,7 +0,0 @@
-# this file is created by me
-
-from django.http import HttpResponse
-
-
-def hello(request):
-    return HttpResponse('<h1>hello world</h1>')
